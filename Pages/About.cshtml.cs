ff7b72ce0714c71c0c25c911b5298babf4d9b188
Selma <seko1800@student.miun.se>
2020-01-23 15:35:16 +0100

    Add project files.

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace moment1._2
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
